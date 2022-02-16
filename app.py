7ee3543d917c4c0836331ea28b47c9b5ee43c056
kjqmiller <kjqmiller@gmail.com>
2022-02-16 11:25:14 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,83 @@
+import json
+from flask import Flask, render_template, url_for, request, redirect
+from helpers import query_imdb, save_to_db, get_all_titles, delete_from_db
+import pymongo
+from pprint import pprint
+
+app = Flask(__name__)
+
+
+# TO START
+# source env/bin/activate
+
+# ROUTES
+# HOME ROUTE
+# TODO Contains search button, will eventually have this at the top of every page along with other nav buttons
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('index.html')
+
+
+# COLLECTION ROUTE
+@app.route('/collection', methods=['GET'])
+def collection():
+    titles = get_all_titles()
+    return render_template('collection.html', titles=titles)
+
+
+# RESULTS ROUTE
+# Render page with results from search, letting the user select whichever titles they want
+@app.route('/results', methods=['POST'])
+def results():
+    if request.method == 'POST':
+        # Get text entered in form with text input and run search function
+        search_query = request.form['search_query']
+
+        # If nothing is entered, reload page, else load results.html and pass the returned titles as 'title'
+        if search_query == '':
+            return render_template('index.html', empty=True)
+        else:
+            query_return = query_imdb(search_query)
+            return render_template('results.html', titles=query_return)
+
+
+# SUBMIT ROUTE
+# Handles submission of form from results route, redirects back to home page after success or failure
+@app.route('/submit', methods=['POST'])
+def submit():
+    titles = []
+    if request.method == 'POST':
+        selected_ids = request.form.getlist('checked_title')
+        # Python string uses single quote ' whereas json uses double ", had to replace to make json.loads() work
+        for item in selected_ids:
+            pprint(item)
+            item = item.replace('\'', '\"')
+            try:
+                title = json.loads(item)
+                titles.append(title)
+            except ValueError:
+                return render_template('index.html', success=2)
+    try:
+        save_to_db(titles)
+        return render_template('index.html', success=0)
+    except Exception:
+        return render_template('index.html', success=1)
+
+
+# DELETE ROUTE
+@app.route('/delete', methods=['POST'])
+def delete():
+    # Get title id and pass to pymongo delete helper function
+    try:
+        if request.method == 'POST':
+            title_to_delete = request.form.get('delete')
+            title_to_delete = title_to_delete.replace('\'', '\"')
+            pprint(title_to_delete)
+            delete_from_db(title_to_delete)
+        return render_template('index.html', deleted=True)
+    except Exception:
+        return render_template('index.html', deleted=False)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Errors pop up on page
---
+++ helpers.py
@@ -0,0 +1,80 @@
+import certifi
+import requests
+from pprint import pprint
+from config import *
+from pymongo import MongoClient
+
+
+def mongo_client_connect():
+    client = MongoClient(
+        mongo_string,
+        # The following not in mongodb atlas docs, need to install and import certifi and add next line
+        tlsCAFile=certifi.where()
+    )
+    print('Connected to client')
+    return client
+
+
+def query_imdb(search_query):
+    # TODO add all sensitive data to a git-ignore file and pass
+    url = imdb_url
+    querystring = {"q": search_query}
+    headers = imdb_headers
+
+    # Get the response from the api and retrieve the json from it
+    r = requests.request("GET", url, headers=headers, params=querystring)
+    json_data = r.json()
+
+    # Get rid of unnecessary keys
+    keys_to_remove = ['@meta', '@type', 'query', 'types']
+    for key in keys_to_remove:
+        json_data.pop(key, None)
+
+    # Create list of items from 'results' key, only adding films and tv shows by checking for 'titleType' key
+    titles = []
+    for item in json_data['results']:
+        if 'titleType' in item:
+            print(item)
+            titles.append(item)
+
+    return titles
+
+
+def save_to_db(checked_titles):
+    client = mongo_client_connect()
+    db = client.movie_collection
+    movies = db.movies
+
+    # Check if title exists, limit=1 stops search as soon as a match is found, no need to keep searching
+    for title in checked_titles:
+        if movies.count_documents({'id': title['id']}, limit=1):
+            print('Title already exists')
+        else:
+            movies.insert_one(title)
+            print('Title inserted')
+
+
+def delete_from_db(movie_id):
+    # Delete selected title from db
+    client = mongo_client_connect()
+    db = client.movie_collection
+    movies = db.movies
+
+    movies.delete_one({'id': movie_id})
+
+
+def get_all_titles():
+    titles = []
+
+    client = mongo_client_connect()
+    db = client.movie_collection
+    collection = db.movies
+
+    # Alphabetize dicts in array by title before returning
+    cursor = collection.find({})
+    for item in cursor:
+        titles.append(item)
+
+    sorted_titles = sorted(titles, key=lambda i: i['title'])
+
+    return sorted_titles
